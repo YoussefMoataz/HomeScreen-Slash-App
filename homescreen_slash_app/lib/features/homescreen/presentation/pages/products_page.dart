5b7f3c504f387172be00ffd92c934139c4f03922
Youssef Moataz <yousefcrunshy03@gmail.com>
2024-06-20 11:57:05 +0300

    Add bottom navigation.

--- homescreen_slash_app/lib/features/homescreen/presentation/pages/products_page.dart
+++ homescreen_slash_app/lib/features/homescreen/presentation/pages/products_page.dart
@@ -1,29 +1,63 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:homescreen_slash_app/features/homescreen/presentation/bloc/categories/categories_cubit.dart';
 import 'package:homescreen_slash_app/features/homescreen/presentation/views/categories_view.dart';
 import 'package:homescreen_slash_app/features/homescreen/presentation/views/products_view.dart';
-import 'package:homescreen_slash_app/features/homescreen/presentation/widgets/categories_section.dart';
-
-import '../bloc/products/products_cubit.dart';
-import '../widgets/products_section.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              CategoriesView(),
-              ProductsView(),
-            ],
+    return PersistentTabView(
+      navBarHeight: 70,
+      tabs: [
+        _buildNavigationBarItem(
+          title: "Home",
+          iconData: CupertinoIcons.home,
+          screen: const SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  CategoriesView(),
+                  ProductsView(),
+                ],
+              ),
+            ),
           ),
         ),
+        _buildNavigationBarItem(
+          title: "Favourites",
+          iconData: CupertinoIcons.heart,
+          screen: Center(child: Text("Favourites screen")),
+        ),
+        _buildNavigationBarItem(
+          title: "My Cart",
+          iconData: CupertinoIcons.shopping_cart,
+          screen: Center(child: Text("My Cart screen")),
+        ),
+        _buildNavigationBarItem(
+          title: "Profile",
+          iconData: CupertinoIcons.person,
+          screen: Center(child: Text("Profile screen")),
+        ),
+      ],
+      navBarBuilder: (navBarConfig) =>
+          Style4BottomNavBar(navBarConfig: navBarConfig),
+    );
+  }
+
+  PersistentTabConfig _buildNavigationBarItem(
+      {required String title,
+      required IconData iconData,
+      required Widget screen}) {
+    return PersistentTabConfig(
+      item: ItemConfig(
+        activeForegroundColor: Colors.black,
+        title: title,
+        icon: Icon(iconData),
       ),
+      screen: screen,
     );
   }
 }
